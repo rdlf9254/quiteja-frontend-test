7abf2b2ae6a0459aaaff85657534519403afdbcc
rodolfo9254 <rodolfo9254@gmail.com>
2024-11-30 15:40:05 -0300

    not found and router to user button added

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import UserList from '@/views/UserList.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import UserList from "@/views/UserList.vue";
+import NotFound from "@/views/NotFound.vue"; // Importa o componente de erro
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/usuarios',
-    name: 'UserList',
-    component: UserList
+    path: "/usuarios",
+    name: "UserList",
+    component: UserList,
   },
-]
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>404 - Página não encontrada</h1>
+    <p>Desculpe, a página que você está tentando acessar não existe.</p>
+    <v-btn color="primary" dark to="/usuarios">
+      Clique aqui para ir para a página de usuários
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style></style>
