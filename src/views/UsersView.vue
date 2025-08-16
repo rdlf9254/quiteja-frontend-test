a9d5bbd8b3e3dc9179e45d94b5cdf83b0ad711b3
Rodolfo <rodolfo.costacurta@inteliger.com.br>
2025-08-16 01:39:29 -0300

    created users view and import users store module

---
+++ src/views/UsersView.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <h1 class="mb-4">Lista de Usuários</h1>
+
+    <div v-if="isLoading" class="text-center">
+      <v-progress-circular
+        indeterminate
+        color="primary"
+        :size="70"
+      ></v-progress-circular>
+      <p class="mt-4">Carregando usuários...</p>
+    </div>
+
+    <v-alert v-else-if="error" type="error">
+      {{ error }}
+    </v-alert>
+
+    <user-list v-else :users="allUsers"></user-list>
+
+  </v-container>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+import UserList from '@/components/users/UserList.vue';
+
+export default {
+  name: 'UsersView',
+  components: {
+    UserList,
+  },
+  computed: {
+    ...mapGetters('users', ['allUsers', 'isLoading', 'error']),
+  },
+  methods: {
+    ...mapActions('users', ['fetchUsers']),
+  },
+  created() {
+    this.fetchUsers();
+  },
+};
+</script>
