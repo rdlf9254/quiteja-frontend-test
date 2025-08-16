23b0b908e1dcf45492c27d534c5ef70b0b2e0213
Rodolfo <rodolfo.costacurta@inteliger.com.br>
2025-08-16 04:14:07 -0300

    improved banner

--- src/components/users/UsersBanner.vue
+++ src/components/users/UsersBanner.vue
@@ -1,13 +1,31 @@
 <template>
-    <v-card>
-        <v-card-title>
-            <h1>Usuários</h1>
-        </v-card-title>
-    </v-card>
+  <v-card>
+    <v-card-title>
+      <div class="flex row align-center gap-5">
+        <img
+          :src="usersImage"
+          alt="Users"
+          class="mr-5"
+        />
+        <h1 class="text-h4 font-weight-bold mr-5">Usuários</h1>
+        <v-divider vertical class="mr-5"></v-divider>
+        <p>
+          <!-- <strong>Total de usuários:</strong> {{ users.length }} -->
+        </p>
+      </div>
+    </v-card-title>
+  </v-card>
 </template>
 
 <script>
+import usersImage from "@/assets/users.svg";
+
 export default {
-    name: 'UsersBanner',
-}
+  name: "UsersBanner",
+  data() {
+    return {
+      usersImage,
+    };
+  },
+};
 </script>
