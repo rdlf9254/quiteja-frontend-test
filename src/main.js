fa1055528505bf13c802dbae4e758f031084bc4d
Rodolfo <rodolfo.costacurta@inteliger.com.br>
2025-08-16 02:19:58 -0300

    added vuetify

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+const vuetify = new Vuetify({})
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
