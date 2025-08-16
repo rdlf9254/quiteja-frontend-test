09605e276cde0d7dee983f351f092b7fe00259b6
Rodolfo <rodolfo.costacurta@inteliger.com.br>
2025-08-16 05:21:46 -0300

    better theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,18 +5,27 @@ import "vuetify/dist/vuetify.min.css";
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  theme: {
-    dark: true, 
-    themes: {
-      light: {
-        primary: '#1976D2',
-        accent: '#82B1FF',
-        secondary: '#424242',
-      },
-      dark: {
-        primary: '#2196F3',
-        accent: '#FF4081',
+    theme: {
+      dark: true,
+      themes: {
+        light: {
+          primary: '#3D5AFE',
+          secondary: '#616161',
+          accent: '#E91E63',
+          error: '#D32F2F',
+          info: '#1976D2',
+          success: '#388E3C',
+          warning: '#FBC02D',
+        },
+        dark: {
+          primary: '#536DFE',
+          secondary: '#B0BEC5',
+          accent: '#F06292',
+          error: '#E57373',
+          info: '#64B5F6',
+          success: '#81C784',
+          warning: '#FFD54F',
+        },
       },
     },
-  },
-});
+  });
